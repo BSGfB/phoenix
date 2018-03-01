e9b51ff69be276803142b3433e8e0f89757f1e43
Siarhei Blashuk <bloshuk74@gmail.com>
2018-03-01 14:46:15 +0300

    Added initial configuration

---
+++ rest/src/main/java/com/phoenix/shop/rest/PhoenixShopApplication.java
@@ -0,0 +1,9 @@
+package com.phoenix.shop.rest;
+
+import org.springframework.boot.SpringApplication;
+
+public class PhoenixShopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PhoenixShopApplication.class, args);
+    }
+}
---
+++ rest/src/main/java/com/phoenix/shop/rest/controller/CityController.java
@@ -0,0 +1,8 @@
+package com.phoenix.shop.rest.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CityController {
+
+}
